7f9a9edcc8596d1ee4574958cd5d8e0aa7c377a7
Kassandra Perch <the@nodebotani.st>
2018-12-23 14:18:14 -0600

    Basic Ace business card

--- build-card.js
+++ build-card.js
@@ -15,6 +15,15 @@ const boxenOptions = {
   borderStyle: 'round'
 }
 
-const output = chalk.blue('Hello, Twitch!')
+const newLine = '\n'
+
+const output = chalk.blue('Hello, Twitch! I am Ace the 😼') + newLine +
+chalk.white('Favorite Food:') + chalk.blue('Plastic Wrap!') + newLine + 
+chalk.green('This is from a tutorial about making '+ newLine + 'your own npx cards') + newLine +
+chalk.magenta('The tutorial can be found at :') + newLine +
+chalk.blue.bold('https://github.com/nodebotanist/npx-business-card-howto') + newLine +
+chalk.white('and this was all written on Twitch and with 💜 by ') + newLine +
+chalk.yellow('nodebotanist ') + newLine +
+chalk.green('run `npx nodebotanist` to learn more!')
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.magenta(boxen(output, boxenOptions)))
