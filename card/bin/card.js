c42cca4189cfbe4ebf982794b50ee174319692d2
Kassandra Perch <the@nodebotani.st>
2018-12-23 14:02:27 -0600

    Cleaned up gh-pages branch

--- card/bin/card.js
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/node
-
-
-// require native Node.js modules
-const path = require('path')
-const fs = require('fs')
-
-const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
-console.log(output)
